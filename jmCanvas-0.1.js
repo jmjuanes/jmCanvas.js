80025332bcc76b425f2b5b1100bd2cc5873a97ca
Josemi Juanes <soporte.jmjuanes@hotmail.es>
2014-03-10 17:53:40 +0100

    Create jmCanvas-0.1.js

---
+++ jmCanvas-0.1.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * jmCanvas.js 
+ * @copyright (C) 2014 Jose Miguel Juanes http://www.jmjuanes.com.es
+ * @version 0.1
+ * @license jmCanvas.js is under Apache 2.0 License.
+ *
+ **/
+
+var jmCanvas = new function ()
+{
+	
+};
