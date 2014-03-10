f9b1c59fa447f42ccf4a51eed4338dc4236899fb
Josemi Juanes <soporte.jmjuanes@hotmail.es>
2014-03-10 19:43:40 +0100

    Update jmCanvas

--- jmCanvas-0.1.js
+++ jmCanvas-0.1.js
@@ -7,7 +7,188 @@
  *
  **/
 
-var jmCanvas = new function ()
+function jmCanvas(nombre, anchura, altura)
 {
+	// nombre:  Nombre del canvas (string)
+	// anchura: Anchura del canvas (entero)
+	// altura:  Altura del canvas (entero)
 	
+	//Identificadores del canvas
+	this.id = null;        //ID del canvas
+	this.ctx = null;       //CTX del canvas
+	this.name = nombre;    //Etiqueta name del canvas
+	this.anc = anchura;    //Anchura del canvas
+	this.alt = altura;     //Altura del canvas
+	
+	//Funcion inicial
+	this.Iniciar = function()
+	{		
+		//Comprobamos si esta disponible
+		if(document.getElementById(this.name))
+		{
+			//Guardamos el ID
+			this.id = document.getElementById(this.name);
+			
+			//Comprobamos si podemos inicializar el canvas
+			if (this.id.getContext)
+			{
+				//Si lo hemos podido inicializar
+				this.ctx = canvas.getContext('2d');
+				if(this.ctx == null)
+				{
+					//Avisamos de que se ha producido un error
+					alert('Se ha producido un error al iniciar el canvas.');
+				}
+				else
+				{
+					//Establecemos la altura y la anchura del canvas
+					this.id.width  = this.anc;
+					this.id.height = this.alt;
+				}
+			}
+		}
+		else
+		{
+			//Si no esta disponible
+			alert("Error al iniciar el canvas " + n + ".");
+		}
+	};
+	
+	//Funcion que borra un recuadro de la pantalla
+	this.Borrar = function(px, py, w, h)
+	{
+		// px: posicion x
+		// py: posicion y
+		// w : anchura
+		// h : altura
+		this.ctx.clearRect(px, py, w, h);
+	};
+	
+	//Funcion que borra toda la pantalla
+	this.BorrarTodo = function()
+	{
+		this.ctx.clearRect(0, 0, this.anc, this.alt);
+	};
+	
+	//Funcion para posicionar una imagen en el canvas
+	this.Imagen = function(img, px, py, af)
+	{
+		// img : imagen a posicionar
+		// px  : posicion x de la imagen
+		// py  : posicion y  de la imagen
+		// af  : alfa a aplicar
+		
+		//Activamos el alfa
+		this.ctx.globalAlpha = af;
+		//Iniciamos
+		this.ctx.beginPath();
+		//Situamos la imagen
+		this.ctx.drawImage(img,px,py);
+		//Cerramos
+		this.ctx.closePath();
+		//Aplicamos
+		this.ctx.fill();
+		//Devolvemos el alfa a su valor inicial
+		this.ctx.globalAlpha = 1;
+	};
+	
+	//Función para rotar una imagen y situarla
+	this.ImagenRotar = function(img, px, py, ang, af)
+	{
+		// img : imagen a posicionar
+		// px  : posicion x de la imagen
+		// py  : posicion y de la imagen
+		// ang : angulo a rotar (en grados)
+		// af  : alfa a aplicar
+		
+		var rad = Math.PI/180;
+		
+		//Guardamos el estado actual
+		this.ctx.save();
+		//Cambiamos el alfa
+		this.ctx.globalAlpha = af;
+		//Iniciamos
+		this.ctx.beginPath();
+		//Trasladamos el centro de la rotacion al centro de la imagen
+		this.ctx.translate(px +(img.width/2) ,py +(img.height/2));
+		//Rotamos
+		this.ctx.rotate(rad*ang);
+		//Situamos la imagen
+		this.ctx.drawImage(img, -(img.width/2), -(img.height/2));
+		//Guardamos
+		this.ctx.closePath();
+		//Aplicamos
+		this.ctx.fill();
+		//Devolvemos al estado anterior
+		this.ctx.restore();
+		//Reiniciamos el alfa
+		this.ctx.globalAlpha = 1;
+	};
+	
+	//Función para posicionar texto
+	this.Texto = function(txt, fuente, color, tam, align, px, py)
+	{
+		// txt    : texto
+		// color  : color del texto
+		// fuente : fuente
+		// tam    : tamano del texto (entero)
+		// align  : alineacion del texto(center|end|left|right|start)
+		// px     : posicion x del texto
+		// py     : posicion y del texto
+		
+		//Baseline
+		this.ctx.textBaseline = 'top';
+		//Definimos el color
+		this.ctx.fillStyle = color;
+		//Definimos el estilo del texto
+		this.ctx.font = tam + 'px ' + fuente;
+		//Alineacion del texto
+		this.ctx.textAlign = align; 
+		//Posicionamos
+		this.ctx.fillText(txt, px, py);
+	};
+	
+	//Función que dibuja un rectangulo
+	this.Rectangulo = function(color, px, py, w, h, af)
+	{
+		// color   : color del rectangulo
+		// px y py : posicion x e y del rectangulo
+		// w y h   : anchura y altura del rectangulo
+		// af      : alfa del rectangulo
+		
+		//Cambiamos el alfa
+		this.ctx.globalAlpha = af;
+		//Establecemos el color
+		this.ctx.fillStyle = color;
+		//Iniciamos
+		this.ctx.beginPath();
+		//Creamos el rectangulo
+		this.ctx.rect(px, py, w, h);
+		//Cerramos
+		this.ctx.closePath();
+		this.ctx.fill();
+		//Devolvemos el alfa a 1
+		this.ctx.globalAlpha = 1;
+	};
+	
+	//Función que crea una recta
+	this.Recta = function(color, tam, ox, oy, fx, fy, af)
+	{
+		// color   : color de la recta
+		// tam     : tamano de la recta
+		// ox y oy : posicion x e y original de la recta
+		// fx y fy : posicion x e y finales de la recta
+		// af      : alfa de la recta
+		
+		this.ctx.globalAlpha = af;
+		this.ctx.beginPath();
+		this.ctx.lineWidth = tam;
+		this.ctx.moveTo(ox,oy);
+		this.ctx.lineTo(fx,fy);
+		this.ctx.strokeStyle = color;
+		this.ctx.stroke();
+		this.ctx.closePath();
+		this.ctx.globalAlpha = 1;
+	};
+
 };
